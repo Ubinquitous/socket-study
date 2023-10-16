baa1db9e74a1c30dd45f3e201dcc2b26754cf45a
Ubinquitous <ubinquitous1@gmail.com>
2023-10-16 10:30:51 +0900

    mac : ipv4 주석 추가

--- macOS/ipv4.cpp
+++ macOS/ipv4.cpp
@@ -6,10 +6,16 @@ using namespace std;
 int main()
 {
     struct in_addr addr;
+    // 12.34.56.78
     addr.s_addr = htonl(12 << 24 | 34 << 16 | 56 << 8 | 78);
+
+    // IPv4 주소를 문자열로 변환
     cout << inet_ntoa(addr);
 
+    // 문자열을 IPv4 주소로 변환
     in_addr_t naddr = inet_addr("192.168.34.0");
+
+    // 호스트 방식 바이트 정렬로 변환
     uint32_t haddr = ntohl(naddr);
     
     cout 
