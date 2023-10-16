a7f6083a6e0ea4185907085cc87f5386bf629af5
Ubinquitous <ubinquitous1@gmail.com>
2023-10-16 10:30:42 +0900

    mac : 바이트정렬 주석 추가

--- macOS/byte_sort.cpp
+++ macOS/byte_sort.cpp
@@ -8,7 +8,12 @@ int main()
     unsigned int idata = 0x12345678;
     unsigned short sdata = 0x1234;
 
+    // htonl => host to network (long type)
+    // 4바이트 정수를 네트워크 바이트 정렬로 변환 후 출력
     cout << "host : " << idata << " " << "network : " << htonl(idata) << std::endl;
+    
+    // htons => host to network (short type)
+    // 2바이트 정수를 네트워크 바이트 정렬로 변환 후 출력
     cout << "host : " << sdata << " " << "network : " << htons(sdata) << std::endl;
 
     return 0;
